eaa356bbb168a4d0309e955e831a385538829de9
d-otero <doterophotography@gmail.com>
2017-04-17 09:38:06 -0400

    5th commit
    Into final adjustments. Transition of nav, social media buttons, and
    browser compatibility

--- js/jquery.js
+++ js/jquery.js
@@ -43,24 +43,37 @@ $(document).ready(function(){
          responsiveBaseWidth: window,
            lazyLoad : true,
             addClassActive : true,
-            center: false,
+            center: true,
             loop: true,
             touchDrag: true,
             mouseDrag: true,
             pullDrag: true,
-            freeDrag: true
+            freeDrag: true,
+            pagination: true,
+            slideSpeed : 600,
+            paginationSpeed: 1000
          });
+         //Following code would do the trick.
+ $(document.documentElement).keyup(function (event) {
+         var owl = $('.owl-carousel');
+   if (event.keyCode == 37) {
+       owl.trigger('owl.prev');
+   }
+   else if (event.keyCode == 39) {
+        owl.trigger('owl.next');
+   }
+
+ });
 	$('#fullpage').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
     animateAnchor: true,
     menu: '#menu',
-    scrollingSpeed: 1000,
-    scrollOverflow: false,
-    scrollOverflowReset: true,
+    scrollingSpeed: 1200,
 		autoScrolling: true,
     horizontalScrolling: false,
 		fitToSection: true,
 		fitToSectionDelay: 200,
+    resetSliders: true,
     loopTop: true,
     loopBottom: true,
     offsetSections: false,
@@ -68,10 +81,12 @@ $(document).ready(function(){
 		easingcss3: 'ease',
     sectionSelector: '.section',
     normalScrollElements: '.item.a, .item.e, .gallery-wrapper, .slideshow-wrapper',
-    touchSensitivity: 1,
+    touchSensitivity: 50,
+    keyboardScrolling: true,
         css3: true,
   autoHeight: true,
-  responsiveWidth: 1200,
-  recordHistory: false
+  responsiveWidth: 1023,
+  recordHistory: false,
+  mouseWheelScrolling: true
     });
 });
